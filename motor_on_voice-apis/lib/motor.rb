0478bfe67afb4890b8c4c9c7bee45f9ec99288b5
MLHustMaroon <doxuan.thanh@monstar-lab.com>
2019-08-23 19:14:16 +0900

    save code

--- motor_on_voice-apis/lib/motor.rb
+++ motor_on_voice-apis/lib/motor.rb
@@ -5,24 +5,13 @@ class Motor
     @client = nil
   end
 
-  def self.init_mqtt
-    if @client.nil?
-      conn_opts = {
-        remote_host: ENV["MQTT_HOST"],
-        remote_port: ENV["MQTT_PORT"],
-        username: ENV["MQTT_USERNAME"],
-        password: ENV["MQTT_PASSWORD"]
-      }
-      @client = MQTT::Client.connect(conn_opts)
-    end
-  end
-
   def self.on(status)
     @running = status
-    message = status ? "running" : "stopped"
+    message = status ? "Running" : "Stopped"
+    client = MqttClient.instance.client
     topic = ENV["MQTT_TOPIC"]
     self.init_mqtt
-    @client.publish(topic, message)
+    client.publish(topic, message)
   end
 
   def self.running?
--- motor_on_voice-apis/server.rb
+++ motor_on_voice-apis/server.rb
@@ -1,6 +1,8 @@
 require 'bundler'
 require './lib/motor'
+require './lib/mqtt_client'
 
+MqttClient._load
 Bundler.require
 post '/' do
   Ralyxa::Skill.handle(request)
